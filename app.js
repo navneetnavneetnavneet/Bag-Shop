ad5d3b831292d1ef1e3e41286276ec3ae1c0fa6d
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2024-07-30 00:00:08 +0530

    error handling and database setup completed successfully

---
+++ app.js
@@ -0,0 +1,30 @@
+require("dotenv").config({
+  path: "./.env",
+});
+const express = require("express");
+const app = express();
+const logger = require("morgan");
+const ErrorHandler = require("./utils/ErrorHandler");
+const { generatedErrors } = require("./middlewares/errors");
+
+// db connection
+require("./models/db").connectDatabase();
+
+// logger
+app.use(logger("tiny"));
+
+// routes
+app.use("/users", require("./routes/userRoutes"));
+app.use("/owners", require("./routes/ownerRoutes"));
+app.use("/products", require("./routes/productRoutes"));
+
+// error handling
+app.all("*", (req, res, next) => {
+  next(new ErrorHandler(`Requested URL Not Found ${req.url}`, 404));
+});
+app.use(generatedErrors);
+
+// creating server
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
